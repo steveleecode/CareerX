0098f5cea3c2458f3016b0f99ccad2bf3a9e0304
steveleecode <stevelee_2008@outlook.com>
2024-08-05 00:08:40 -0700

    Finished Updated Home Page

--- app/src/main/java/com/example/careerx_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/careerx_kotlin/MainActivity.kt
@@ -1,16 +1,34 @@
 package com.example.careerx_kotlin
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.view.WindowInsetsController
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar);
+        getSupportActionBar()?.setTitle("CareerX");
+
+        val imageview = findViewById<ImageView>(R.id.imageView)
+        imageview.setImageResource(R.drawable.smile)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        }
 
         val startSurveyButton: Button = findViewById(R.id.start_survey_button)
         startSurveyButton.setOnClickListener {
@@ -19,3 +37,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+
